0d9b0f14aecb7b87936e13370476f72932444e98
Seanna Qin <seannaq@cs.washington.edu>
2026-02-03 16:06:06 -0800

    created backend folder

---
+++ Backend/timer.ts
@@ -0,0 +1,20 @@
+class Timer {
+
+  constructor() {
+    let seconds: number = 60;
+    let startTime;
+  }
+
+  public start() {
+
+  }
+
+  public stop(): void {
+
+  }
+
+  public reset() {
+
+  }
+
+}
